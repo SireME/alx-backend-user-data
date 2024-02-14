bb408f31e9d14011a29656c9e8149ef9e0e8daf4
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-14 13:11:37 +0100

    add endpoint to test unauthorized register

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def not_authorized() -> None :
+    """ GET /api/v1/unauthorized
+    trigger unauthorized endpoint
+    """
+    abort(401)
