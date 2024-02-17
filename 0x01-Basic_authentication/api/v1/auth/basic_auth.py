76ff49cdc862c1f276e4600463b829867b7952be
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-17 19:06:57 +0100

    create basic auth template

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for user authentication"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    This module implements basic authentication
+    """
+    pass
