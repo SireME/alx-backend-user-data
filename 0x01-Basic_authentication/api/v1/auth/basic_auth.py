0958300b87f079a172f8a24937481b7973601b1c
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-18 17:06:54 +0100

    extract Authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,16 @@ class BasicAuth(Auth):
     """
     This module implements basic authentication
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        method to return authorization header
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(' ')[1]
