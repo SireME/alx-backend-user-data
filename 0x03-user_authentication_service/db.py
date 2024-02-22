56dc1ac5561096ec4072f51ef76a84549311c074
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-22 01:48:06 +0100

    implement find user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,8 +50,9 @@ class DB:
         from sqlalchemy.orm.exc import NoResultFound
 
         try:
-            return self._session.query(User).filter_by(**kwargs).one()
-        except NoResultFound:
-            raise NoResultFound
+            user = self.session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
         except InvalidRequestError:
-            raise InvalidRequestError
+            raise
