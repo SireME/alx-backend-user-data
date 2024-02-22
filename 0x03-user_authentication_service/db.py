ab935b8c133252dd25016d49e733486b46b733b7
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-22 02:37:14 +0100

    add update user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,3 +54,14 @@ class DB:
             return user
         except InvalidRequestError:
             raise
+
+    def update_user(self, user_id, **kwargs) -> None:
+        """
+        This method updates a user based on its passed attributes
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in user.__dict__:
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
