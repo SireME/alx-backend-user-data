1444a8768be4b8a8f830a5c602a93efb4b5f186f
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-22 02:09:35 +0100

    fix session reference

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,7 +43,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """
         return first row of user based on keyword arguments
         """
