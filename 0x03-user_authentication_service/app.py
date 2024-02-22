c854222c47d92e2afc07006d1379913ab19cf2cb
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-22 04:28:57 +0100

    basic flask applpication

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module contains a basic flask application
+"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome() -> str:
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
