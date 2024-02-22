2cdbf7f58d41561a644c4f474475344fbc958387
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-22 06:56:14 +0100

    implement login session endpoint and helper functions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 This module contains a basic flask application
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 AUTH = Auth()
 
@@ -30,6 +30,23 @@ def users() -> str:
     except ValueError:
         r = {"message": "email already registered"}
         return jsonify(r), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """
+    authenticate and login user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    msg = {"email": email, "message": "logged in"}
+    response = jsonify(msg)
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
