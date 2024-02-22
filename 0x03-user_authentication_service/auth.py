ceb9b310c095c33558ce49a2358380953693b52d
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-22 04:07:28 +0100

    register user todb with hashed password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 This module handles user suthentication
 """
 from user import User
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +13,25 @@ def _hash_password(password: str) -> bytes:
     used from the User class
     """
     return User.set_password(User, password)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        This method registers a user to the application
+        """
+        try:
+            u = self._db.find_user_by(email=email)
+            if u:
+                message = f'User {email} already exists'
+                raise ValueError(message)
+        except NoResultFound:
+            hp = _hash_password(password)
+            obj = self._db.add_user(email=email, hashed_password=hp)
+            return obj
